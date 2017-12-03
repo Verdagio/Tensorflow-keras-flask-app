36f4c112370f8d10edd62a1038b9c84c2517566e
Verdagio <dverdagio@gmail.com>
2017-12-03 16:50:36 +0000

    Values being returned

--- static/js/draw.js
+++ static/js/draw.js
@@ -14,10 +14,12 @@ var hold;
 
 function addClick(x, y, dragging)
 {
-    posX.push(x);
-    posY.push(y);
-    posDrag.push(dragging);
+    posX.push(x); //push data to the x position array
+    posY.push(y); // push data to the y position array
+    posDrag.push(dragging); // push data to the drag positions array
 }
+// The mousedown event occurs when the left mouse button is pressed down over the selected element.
+// ref: https://www.w3schools.com/jsref/event_onmousedown.asp
 canvas.addEventListener('mousedown', function(e) {
     canvas.style.cursor = "pointer";    
     var mouseX = e.pageX - canvas.offsetLeft;
@@ -29,7 +31,7 @@ canvas.addEventListener('mousedown', function(e) {
 
 });
 // The mouseup e is sent to an element when the mouse pointer is over the element, and the mouse button is released. 
-// ref: https://www.w3schools.com/jsref/e_onmouseup.asp 
+// ref: https://www.w3schools.com/jsref/event_onmouseup.asp 
 canvas.addEventListener('mouseup', function (e){
     canvas.style.cursor = "default";   
     hold = false;
@@ -56,9 +58,9 @@ canvas.addEventListener("mouseout", function (e){
 function redraw(canvas){ // Draw a line on the canvas using the x & y positions
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Clears the canvas
     
-    ctx.strokeStyle = "#ffffff";
-    ctx.lineJoin = "round";
-    ctx.lineWidth = 20;
+    ctx.strokeStyle = "#ffffff"; //set colour
+    ctx.lineJoin = "round"; // set line end
+    ctx.lineWidth = 15;     // set width
               
     for(var i=0; i < posX.length; i++) {		
         ctx.beginPath();
@@ -77,15 +79,11 @@ function redraw(canvas){ // Draw a line on the canvas using the x & y positions
 function reset(){ // clear the rectangle so that the user can draw again
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height); // Clears the canvas
 }
-// currently not working will fix later...
 
 function sendFile(){
 
     //first save the file as a png - will be saved as a base 64 image
     var img = canvas.toDataURL('image/png');
-
-
-    //$SCRIPT_ROOT = request.script_root|tojson|safe;
 
     //create an ajax call to the python script
     $.ajax({    // sent base 64 image to python as a json file.
@@ -94,10 +92,42 @@ function sendFile(){
         data: img ,
         success: function(res){     // return the result            
             console.log(res);
-            
-            
+            $('#result').text('Prediction: '+res);
         },error: function(err){     // return the error
             console.log(err);
+            $('#result').text('Oops.... something went wrong :( ' + err);
         }
     });    
 }
+
+// {/* <input class="form-control" width=""id="imgToUpload" type=file name=file/>
+
+// <button type='button' id="uploadImg01" class="btn btn-block text-white">Upload</button>
+
+// Then the javascript for when the button works:
+//   $('#uploadImg01').click(function ( event ){
+//     event.preventDefault();
+     
+//     var file = {
+//       imageFileName: "img",
+//       base64: "" + imageBase64 // Turns Base64 image into string
+//     }
+
+//     file = JSON.stringify(file); // Turns whole json object into string
+    
+//     $.ajax({
+//           url: '/save',
+//           headers: {
+//             'Content-Type':'application/json' // pass as json
+//           },
+//           data: file,
+//           dataType: 'json',
+//           method: 'POST',
+//           success: function (success) {
+//               console.log(success);
+//           },
+//           error: function (error) {
+//             console.log(error);
+//           }
+//         });
+//   }) */}
--- webapp.py
+++ webapp.py
@@ -4,6 +4,7 @@ import os, re
 import base64
 import keras as kr
 import numpy as np
+import tensorflow as tf
 from scipy.misc import imread, imresize
 from flask import Flask, request, redirect, url_for
 from werkzeug.utils import secure_filename
@@ -13,18 +14,28 @@ from werkzeug.utils import secure_filename
 #load up our previously trained model
 
 def init():
-    classes = 10
-    r, c = 28, 28
-    input_shape = (r, c, 1)
 
-    model = kr.models.Sequential()
-    model.add(kr.layers.Dense(512, input_shape=(784,)))
-    model.add(kr.layers.Activation('relu'))
-    model.add(kr.layers.Dropout(0.2))
-    model.add(kr.layers.Dense(10))
-    model.add(kr.layers.Activation('softmax'))
-    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-    model = kr.models.load_weights("./data/mnist_nn.h5")
+    model = kr.models.load_model("./data/mnist_nn.h5")
+    graph = tf.get_default_graph()
+
+    return model, graph
+
+def imageParser(data):
+    # using a regex we find  base64, and take all that follows :)
+    tmp = re.search(b'base64,(.*)', data).group(1)
+    # now that the image data is gone, need to save it as an image to the uploads dir
+    # ref: https://stackoverflow.com/questions/2323128/convert-string-in-base64-to-image-and-save-on-filesystem-in-python
+    with open('./uploads/canvasImg.png','wb') as output:
+       output.write(base64.b64decode(tmp))
+
+def predictNumber(file):
+    model, graph = init()
+    with graph.as_default():
+        prediction = model.predict(file.astype('int')) # try now to predict the model
+        response = np.array_str(np.argmax(prediction))
+    
+    return response
+
 
 
 UPLOAD_FOLDER = './uploads'
@@ -41,14 +52,11 @@ def allowed_file(filename):
 
 @app.route('/uploadFile', methods = ['POST'])
 def uploaded():
-    init()
     #look at the data
     #print(request.get_data())
     #regex the data to extract the file data stuff that we dont need from ivbor...etc.
     imageParser(request.get_data())
-
     img = imread('./uploads/canvasImg.png', mode='L')    # read the file in greyscale
-    img = np.invert(img)                                 # invert the image for added accuracy ;)
     img = imresize(img,(28,28))                          # resize the image to 28 * 28
     # Gives a new shape to an array without changing its data
     newImg = np.ndarray.flatten(np.array(img)).reshape(1, 784).astype('float32')
@@ -57,20 +65,10 @@ def uploaded():
     # divide by 255 to make it 0 or 1
     newImg /= 255
 
-    print(newImg)
-    prediction = model.predict(newImg.astype('int'), batch_size=512)
-    print(prediction)
-    return 'hold on'
+    response = predictNumber(newImg)
 
-
-def imageParser(data):
-    # using a regex we find  base64, and take all that follows :)
-    tmp = re.search(b'base64,(.*)', data).group(1)
-    # now that the image data is gone, need to save it as an image to the uploads dir
-    # ref: https://stackoverflow.com/questions/2323128/convert-string-in-base64-to-image-and-save-on-filesystem-in-python
-    with open('./uploads/canvasImg.png','wb') as output:
-       output.write(base64.b64decode(tmp))#somethings not working here :(
-
+    return response
+ 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return app.send_static_file('index.html')
